0213070a3c3f97f6cfaa386eb0da5a0bacea9b24
EduardMTT <torresrojasb013@gmail.com>
2025-03-02 14:52:56 -0600

    Diseño SLIDER
    He implementado el funcionamiento y diseño del slider en el proyecto, incorporando su estructura visual dentro del componente. Además, optimicé la organización del código al modificar la ubicación de las importaciones de la Navbar y el Footer, moviéndolas al layout principal de Next.js. Con este ajuste, ahora podemos cambiar únicamente el contenido entre estos dos elementos utilizando la propiedad "children"

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@ import { Slider } from "./components/slider";
 //Te elimine este amistad para mejor importar la NAVBAR y el FOOTER en el mismo layout asi ya no tendriamos que importar el contenido en esta page
 
 export default function Home() {
+  //Quiero creer que podemos estructurar aqui cada componente de la landing page para asi solo importar este componente HOME en el layout
   return (
-    //Prueba de slider
-    <center>
-      <Slider />
-    </center>
+    <>
+      <div className="Contenedor-Slider">
+        <span>Elegancia, Lujo y Calidad: La Esencia de Nuestro Trabajo.</span>
+        <Slider />
+      </div>
+    </>
   );
 }
