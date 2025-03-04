2abd29fc034e9ffc664c0aa352d2c45a135fdf08
Brandon Eduardo <torresrojasb013@gmail.com>
2025-03-03 18:07:18 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
 import { AboutUs } from "./components/about-us";
-import { Footer } from "./components/footer";
-import { Navbar } from "./components/navbar";
+import { Slider } from "./components/slider";
 
 export default function Home() {
-  return (
+   return (
     <>
-      <Navbar />
-      <br />
       <AboutUs />
-
-      <Footer />
+      <div className="Contenedor-Slider">
+        <span className="drop-shadow-[3px_3px_3px_rgba(0,0,0,0)]">La Esencia de Nuestro Trabajo.</span>
+        <Slider />
+      </div>
     </>
   );
 }
